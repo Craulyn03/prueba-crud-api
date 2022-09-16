1fb7bdd2d57c32f3943ba4aa8128f7aca4c25ffe
Craulyn <craulinoliver@outlook.com>
2022-09-16 11:26:16 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Flex, Square, Box } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import Menu from "./components/Menu";
 import { Routes, Route } from "react-router-dom";
 import Inicio from "./pages/Inicio";
